f4fc5944ed10b808efcfb181e3a0333f10f82039
mr4dd <158076664+mr4dd@users.noreply.github.com>
2026-04-24 15:02:37 +0100

    transition vers fichiers bootstrap locaux

--- stock_project/settings.py
+++ stock_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -121,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 # Media files
 MEDIA_URL = '/media/'
